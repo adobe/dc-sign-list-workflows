6baf91406db57d2bf8dd3a5e90c50432b31ea6d2
Chris Hughes <cajhughes@gmail.com>
2023-10-06 08:54:44 +0100

    Support optional --sandbox flag for v2.0.0

--- src/main/java/uk/org/esig/adobe/workflows/ListWorkflows.java
+++ src/main/java/uk/org/esig/adobe/workflows/ListWorkflows.java
@@ -18,24 +18,31 @@ import java.util.List;
 import java.util.Map;
 
 public class ListWorkflows {
-    private static final String API_HOST = "https://api.adobesign.com/";
     private static final String API_PATH = "api/rest/v6";
+    private static final String API_URL = "https://api.adobesign.com/";
     private static final String API_USER_PREFIX = "email:";
     private static final String BEARER = "Bearer ";
     private static final int CAPACITY = 20000;
     private static final int PAGE_SIZE = 1000;
+    private static final String SANDBOX = "--sandbox";
+    private static final String SANDBOX_API_URL = "https://api.adobesignsandbox.com/";
     private static final int TIMEOUT = 300000;
-    private static final String USAGE = "Usage: java -jar aas-list-workflows-<version>.jar <integrationKey>";
+    private static final String USAGE = "Usage: java -jar aas-list-workflows-<version>.jar <integrationKey> [--sandbox]";
 
     public static void main(String[] args) {
-        if (args.length != 1) {
+        if (args.length < 1 || args.length > 2) {
             System.out.println(getUsage());
         }
         else {
             String accessToken = BEARER + args[0];
             ListWorkflows list = new ListWorkflows();
             try {
-                list.execute(accessToken);
+                if (args.length == 2 && args[1] != null && args[1].equalsIgnoreCase(SANDBOX)) {
+                    list.execute(SANDBOX_API_URL, accessToken);
+                }
+                else {
+                    list.execute(API_URL, accessToken);
+                }
             }
             catch (ApiException ae) {
                 System.out.println(getExceptionDetails(ae));
@@ -44,12 +51,12 @@ public class ListWorkflows {
         }
     }
 
-    public void execute(String accessToken) throws ApiException {
+    public void execute(String apiUrl, String accessToken) throws ApiException {
         /*
          *  Establish connection to Adobe Sign API, and obtain the correct API Access Point for the account
          */
         ApiClient apiClient = new ApiClient();
-        apiClient.setBasePath(API_HOST + API_PATH);
+        apiClient.setBasePath(apiUrl + API_PATH);
         apiClient.setConnectTimeout(TIMEOUT).setReadTimeout(TIMEOUT);
         BaseUrisApi baseUrisApi = new BaseUrisApi(apiClient);
         BaseUriInfo baseUriInfo = baseUrisApi.getBaseUris(accessToken);
@@ -61,8 +68,8 @@ public class ListWorkflows {
         UsersApi usersApi = new UsersApi(apiClient);
         OriginatorWorkflowsApi workflowsApi = new OriginatorWorkflowsApi(apiClient);
 
-        Map<String, OriginatorWorkflow> foundWorkflows = new HashMap<String, OriginatorWorkflow>(CAPACITY);
-        Map<String, UserInfo> foundUsers = new HashMap<String, UserInfo>(CAPACITY);
+        Map<String, OriginatorWorkflow> foundWorkflows = new HashMap<>(CAPACITY);
+        Map<String, UserInfo> foundUsers = new HashMap<>(CAPACITY);
         /*
          *  Obtain the first page of users
          */
@@ -81,7 +88,6 @@ public class ListWorkflows {
                 foundUsers.put(userInfo.getId(), userInfo);
                 DetailedUserInfo detail = usersApi.getUserDetail(accessToken, userInfo.getId(), null);
                 if (detail != null && detail.getStatus().equals(DetailedUserInfo.StatusEnum.ACTIVE)) {
-                    String userId = userInfo.getId();
                     String email = userInfo.getEmail();
                     String apiUser = API_USER_PREFIX + email;
                     OriginatorWorkflows workflows = workflowsApi.getWorkflows(accessToken,
